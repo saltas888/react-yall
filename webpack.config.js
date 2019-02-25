a6c55535ffe551da2c177ee9c6c92917e36d58f2
Saltaferis Dimitrios <d.saltaferis@gmail.com>
2019-02-26 01:25:45 +0200

    building

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+module.exports = {
+  entry: './src/lib',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
+};
