c643fb96dbaf7e93032a87da54a9622f48ed0986
Saltaferis Dimitrios <d.saltaferis@gmail.com>
2019-06-12 01:05:04 +0300

    docs

---
+++ docs/precache-manifest.230efdf87d6d86f9e8c34643c290c2ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b90b074f344b206120aa",
+    "url": "/react-yall/static/js/runtime~main.b90b074f.js"
+  },
+  {
+    "revision": "25beffc5ce156c59c6fe",
+    "url": "/react-yall/static/js/main.25beffc5.chunk.js"
+  },
+  {
+    "revision": "438386ba5b3c2f5e22e4",
+    "url": "/react-yall/static/js/2.438386ba.chunk.js"
+  },
+  {
+    "revision": "a81c5259371b03c2991770e9b6761288",
+    "url": "/react-yall/index.html"
+  }
+];
